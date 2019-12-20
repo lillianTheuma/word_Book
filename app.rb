3fc8b7d64716533cd0210665fe79dc95665ee978
Student <example@example.com>
2019-12-20 12:40:44 -0800

    Added 'definition adder'

--- app.rb
+++ app.rb
@@ -21,9 +21,13 @@ get('/words/new') do
 end
 
 get('/words/:id') do
+  @word = Word.find(params[:id].to_i())
   erb(:word)
 end
 
+get('/words/:id/new') do
+  erb(:new_definition)
+end
 ### POST
 
 post ('/words') do
@@ -31,6 +35,13 @@ post ('/words') do
   word = Word.new({:word => word, :id => nil})
   word.save()
   redirect to('/words')
+end
+
+post('/words/:id/definitions') do
+  @word = Word.find(params[:id].to_i())
+  definition = Definition.new({:word => params[:word], :id => nil, :word_id => @word.id})
+  definition.save()
+  erb(:word)
 end
 
 ### PATCH
