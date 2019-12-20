ed20cfd9fd6a5722600ce876bd0455d95dd5a6bd
Student <example@example.com>
2019-12-20 09:56:53 -0800

    Added gemfile, app.rb with template requests.

---
+++ app.rb
@@ -0,0 +1,41 @@
+require('sinatra')
+require('sinatra/reloader')
+require('./lib/word')
+require('./lib/definition')
+require('pry')
+also_reload('lib/**/*.rb')
+
+### GET
+
+get('/') do
+  redirect to('/words')
+end
+
+get('/words') do
+  @words = Word.sort
+  erb(:words)
+end
+
+### POST
+
+post ('/words') do
+  name = params[:word_name]
+  word = Word.new({:name => name, :id => nil})
+  word.save()
+  redirect to('/words')
+end
+
+### PATCH
+patch ('/words/:id') do
+  @word = Word.find(params[:id].to_i())
+  @word.update(params[:name])
+  redirect to('/words')
+end
+
+### DELETE
+
+delete ('/words/:id') do
+  @word = Word.find(params[:id].to_i())
+  @word.delete()
+  redirect to('/words')
+end
